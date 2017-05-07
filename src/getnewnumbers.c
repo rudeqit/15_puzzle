f508899f439ceb8afef041bfc421eb810ab91863
SlackBat <dvtlion@gmail.com>
2017-05-08 02:09:50 +0700

    Connect two functions

--- src/getnewnumbers.c
+++ src/getnewnumbers.c
@@ -4,6 +4,53 @@
 #include <stdio.h>
 #include <time.h>
 
+void swp(int *arr)
+{
+    int swap;
+
+    swap = arr[4];
+    arr[4] = arr[7];
+    arr[7] = swap;
+
+    swap = arr[5];
+    arr[5] = arr[6];
+    arr[6] = swap;
+
+    swap = arr[12];
+    arr[12] = arr[14];
+    arr[14] = swap;
+}
+
+int count_couple(int *arr)
+{
+    int count;
+
+    swp(arr);
+
+    for (int i = 0; i < 15; ++i) {
+	for (int j = i; j < 15; ++j) {
+	    if (arr[i] > arr[j]) {
+		++count;
+	    }
+	}
+    }
+
+    swp(arr);
+
+    return count;
+}
+
+int win_check(int *arr)
+{
+//  Вернуть 0, если пятнашки могут быть собраны
+//  и 1, если нет
+    if (count_couple(arr) % 2 == 1) {
+	return 0;
+    } else {
+	return 1;
+    }
+}
+
 int getrand(int min, int max)
 {
     return (double)rand() / (RAND_MAX + 1.0) * (max - min) + min;
@@ -12,44 +59,28 @@ int getrand(int min, int max)
 int get_new_numbers()
 {
     int arr[15], swap1, swap2, save;
-    int rand = getrand(20, 30);
+    int rand;
 
     srand(time(NULL));
 
-//  Сначала заполняем массив числами от 1 до 15
+//  Заполняем массив числами от 1 до 15 по порядку
     for (int i = 0; i < 15; ++i) {
 	arr[i] = i + 1;
     }
 
-//  А затем перемешиваем его случайные элементы
-//  случайное количество раз
-    for (int i = 0; i < rand; ++i) {
-	swap1 = getrand(0, 15);
-	swap2 = getrand(0, 15);
-	save = arr[swap1];
-	arr[swap1] = arr[swap2];
-	arr[swap2] = save;
-    }
+//  Перемешиваем его случайные элементы случайное
+//  количество раз, пока он не станет "собираемым"
+    do {
+	rand = getrand(20, 30);
+
+	for (int i = 0; i < rand; ++i) {
+	    swap1 = getrand(0, 15);
+	    swap2 = getrand(0, 15);
+	    save = arr[swap1];
+	    arr[swap1] = arr[swap2];
+	    arr[swap2] = save;
+	}
+    } while (win_check(arr));
 
     return *arr;
 }
-
-/*
-
-int main()
-{
-    int arr[];
-    int *arr = get_new_numbers();
-
-    for () {
-
-    }
-
-    for (int i = 0; i < 15; ++i) {
-	printf("%d ", arr[i]);
-    }
-
-    return 0;
-}
-
-*/
