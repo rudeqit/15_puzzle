65e45e57998d196d5a099bfae3eba6b1ccdb9824
Rudeqit <antoshapianist@gmail.com>
2017-06-11 13:54:09 +0700

    One more commit for travis

--- test/logic_test.c
+++ test/logic_test.c
@@ -3,13 +3,8 @@
 
 CTEST(check_solvency, correct_win_combination)
 {
-	int arr[15];
-
-	for (int i = 0; i < 16; ++i) {
-		arr[i] = i + 1;
-	}
-	
-	arr[15] = 0;
+	int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+		11, 12, 13, 14, 15, 0};
 
 	const int result = check_solvency(arr);
 
@@ -18,7 +13,7 @@ CTEST(check_solvency, correct_win_combination)
 
 CTEST(check_solvency, correct_combination)
 {
-	int arr[] = {14, 5, 2, 3, 7, 12, 15, 11, 4,
+	int arr[] = {14, 5, 2, 3, 7, 12, 15, 11, 4, 
 		13, 9, 10, 1, 8, 6, 0};
 
 	const int result = check_solvency(arr);
@@ -28,15 +23,8 @@ CTEST(check_solvency, correct_combination)
 
 CTEST(check_solvency, uncorrect_combination)
 {
-	int arr[15];
-
-	for (int i = 0; i < 13; ++i) {
-		arr[i] = i + 1;
-	}
-	
-	arr[15] = 0;
-	arr[14] = 14;
-	arr[13] = 15;
+	int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+		11, 12, 13, 15, 14, 0};
 
 	const int result = check_solvency(arr);
 
